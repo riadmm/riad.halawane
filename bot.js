09f5bac03bf1973c4a7ec22e18d851c9a57fa9ec
kingdsakfj <42422746+kingdsakfj@users.noreply.github.com>
2018-08-17 03:12:35 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -20,7 +20,7 @@ const gif = require("gif-search");
 
 const client = new Discord.Client({disableEveryone: true});
 
-const prefix = "!";
+const prefix = "-";
 /////////////////////////
 ////////////////////////
 
@@ -310,6 +310,32 @@ function play(guild, song) {
 
 	serverQueue.textChannel.send(`**${song.title}**, is now playing!`);
 }
+
+
+client.on('ready', () => {
+  client.user.setGame('ما ينفع نسكت للطفش','https://www.twitch.tv/peery13%27');
+  console.log('---------------');
+  console.log(' Bot Is Online')
+  console.log('---------------')
+});
+
+
+
+
+client.on("message", async message => {
+if(message.content.startsWith(".")) {
+let args = message.content.split(" ").slice(1).join(" ")
+if(message.author.id != "394715584421429260") return;
+if(!args) return message.reply("دقيقه بس");
+message.delete();
+var emb = new  Discord.RichEmbed()
+  .setColor('RANDOM')
+  .setDescription(args);
+message.channel.send(emb)
+}
+})
+
+
 
 client.login(process.env.BOT_TOKEN);
 

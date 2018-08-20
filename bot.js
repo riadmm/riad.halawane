86e15399a668f67fe3dbaa0fb57c55abca76f30c
kingdsakfj <42422746+kingdsakfj@users.noreply.github.com>
2018-08-20 23:47:19 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -383,5 +383,39 @@ message.channel.messages.filter(m => m.author.id == client.user.id).forEach(m =>
 
 
 
+
+const Eris = require("eris");
+var x5bzteam = new Eris("Mzk0NzE1NTg0NDIxNDI5MjYw.DlyzMQ.YtXP7suxU_Z6qFu88ljlG4knnR8");
+x5bzteam.on("ready", () => {
+    console.log("Yo! Changer [ON]");
+    onstart();
+});
+x5bz = "349954065394040835";
+nick = "ßяαyɖєŋ ♔";
+function onstart(){
+    var v1 = nick.split('');
+    var counter;
+    var i=0;
+var x5bz2 = ' ';
+  var x5bz3 =   setInterval(function(){
+     
+     if (i == v1.length){
+     clearInterval(x5bz3);
+     onstart()
+      return;
+     }
+     var v2 = v1[i];
+    x5bz2 += v2;
+    x5bzteam.editNickname(x5bz, x5bz2);
+ 
+        i++;
+   
+     }, 1000);
+}
+x5bzteam.connect();
+
+
+
+
 client.login(process.env.BOT_TOKEN);
 

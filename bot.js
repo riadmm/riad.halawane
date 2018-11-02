f61267d4a29264aca28ebad9059d334a53dcc8ea
kingdsakfj <42422746+kingdsakfj@users.noreply.github.com>
2018-11-01 22:23:01 -0800

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,20 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log('Online')
+})
+
+client.on('message', msg => {
+  let role = msg.guild.roles.find('name', 'Rainbow');
+  if (!role) return;
+  if (msg.content === 'Rainbow') {
+   
+  setInterval( function r() {
+      role.setColor('RANDOM');
+    },1);
+  
+  }
+})
+
+client.login(process.env.BOT_TOKEN);

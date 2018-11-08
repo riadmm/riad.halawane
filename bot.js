5d469992d8b0541a91bb5b93445876fbca930472
kingdsakfj <42422746+kingdsakfj@users.noreply.github.com>
2018-11-08 13:11:57 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -556,59 +556,6 @@ client.on('message', async msg => { // eslint disable line
 
 
 
-    if (message.content === "-id") {
-
-    let embed = new Discord.RichEmbed()
-
-   .setColor("RANDOM")
-
-   .setThumbnail(message.author.avatarURL)
-
-   .setTitle(`info about ${message.guild.name}`)
-
-   .addField("Server Owner 👑",`➥ ` + `${message.guild.owner.user.username}`, true)
-
-   .addField('Server ID 🆔',`➥` + message.guild.id, true)
-
-   .addField("Owner Tag",`➥ ` + `#` + message.guild.owner.user.discriminator, true)
-
-   .addField("Owner ID 🆔",`➥ ` + message.guild.owner.user.id, true)
-
-   .addField("Server Region📡",`➥ ` + message.guild.region, true)
-
-   .addField("Server Member Size🏧",`➥ ` + message.guild.members.size, true)
-
-   .addField("Server Channels Number🏧",`➥ ` + message.guild.channels.size, true)
-
-   .addField("Server Roels Number🏧",`➥ ` + message.guild.roles.size, true)
-
-   .addField("AFK channel💤",`➥ ` + message.guild.afkChannel || 'Null', true)
-
-   .addField("Server Created AT",`➥ ` + message.guild.createdAt, true)
-
-   .addField(`info about ${message.author.username}`, `➥ `)
-
-   .addField("Name",`➥ ` + `${message.author.username}`, true)
-
-   .addField('Tag',`➥ ` + "#" + message.author.discriminator, true)
-
-   .addField("ID 🆔",`➥ ` + message.author.id, true)
-
-   .addField(" Account Created At",`➥ ` + message.author.createdAt, true)
-
-   .setTimestamp()
-
-   .setFooter(message.author.tag, message.author.avatarURL)
-
-      
-
-      
-
-   message.channel.sendEmbed(embed);
-
-     }
-
- });
 
 
 

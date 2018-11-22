947c8deb29d978cfabe4eb1f77c36efea2c25e9a
kingdsakfj <42422746+kingdsakfj@users.noreply.github.com>
2018-11-22 12:21:48 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -299,26 +299,7 @@ client.on('guildMemberAdd', member => {
     
       channel.sendEmbed(embed);
     });
-    
-
-
-
-
-
-   if (message.content === "$help") {
-    let embed = new Discord.RichEmbed()
-  .setAuthor(message.author.username)
-  .setFooter(`© SHYBOY_05 ™.`, 'https://images-ext-2.discordapp.net/external/X9SanEG0s7Dtv3krTgy-kod_fj6JRVJ2AG4JryCiiz0/%3Fsize%3D2048/https/cdn.discordapp.com/avatars/375761288518828042/fffa31c797e88cf059dd6db424ff456a.png?width=80&height=80')
-  .setColor("#000000")
-  .addField("Done | تــــم" , ":envelope: | :sleuth_or_spy::skin-tone-3:شيك علي في الخاص")
-  
-  
-  
-  message.channel.sendEmbed(embed);
-  }
-  });
-  
-  
+   
   
   
   

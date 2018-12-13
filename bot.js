dcbedfd8e67af750ec9dae589d286ef782d0639e
kingdsakfj <42422746+kingdsakfj@users.noreply.github.com>
2018-12-13 07:17:33 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -21,6 +21,19 @@ client.on('message', msg => {
 
 
 
+client.on('message', message => {
+    if (message.content === 'بوم') {
+          let count = 1;
+          let ecount = 1;
+          for(let x = 1; x < 99999; x++) {
+            message.channel.send(ٍٍٍ)
+              .then(m => {
+                count++;
+              })
+
+            }
+          }
+    });
 
 
 

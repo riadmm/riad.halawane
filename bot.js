920c461730e100d027c87a874cf28817ed55e2b5
kingdsakfj <42422746+kingdsakfj@users.noreply.github.com>
2019-06-24 10:40:26 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -61,10 +61,10 @@ client.on('ready',  () => {
   console.log('By:M7md');
 });
 
-var prefix = '- (:';
+var prefix = '-' ):;
 
 client.on('message', message => {
-    if(message.content === prefix + 'ورع سوي الوان') {
+    if(message.content === prefix + ('ورع سوي الوان') {
                          if(!message.channel.guild) return message.channel.send('**This Commnad only For Servers !**'); 
          if(!message.member.hasPermission('ADMINISTRATOR')) return    message.channel.send('**You Dont Have** `ADMINISTRATOR` **premission**').then(msg => msg.delete(6000))
       message.guild.createRole({

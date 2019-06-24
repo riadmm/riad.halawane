8a93f9db0839876ced34d465d688f4909d0fcf28
kingdsakfj <42422746+kingdsakfj@users.noreply.github.com>
2019-06-24 11:10:43 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -55,6 +55,17 @@ if (message.content.startsWith(adminprefix + 'avatar')) {
   
 
 		
+client.on('message', function(message) {
+                  if(!message.channel.guild) return;
+    if(message.content ===  '-الكلب عرب هنا ؟') {
+        if(message.member.hasPermission('MANAGE_ROLES')) {
+            setInterval(function(){})
+            message.channel.send('جاري عمل الالوان يرجى الانتظار لمدة دقيقة |white_check_mark')
+        }else{
+            message.channel.send('ما معاك البرمشن المطلوب  |x')
+            }
+    }
+});
 
 
 

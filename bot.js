41d8bf5185a002b70a55c5f875d0b19d2098e613
kingdsakfj <42422746+kingdsakfj@users.noreply.github.com>
2019-06-24 11:52:27 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -63,7 +63,7 @@ client.on('ready',  () => {
 var prefix = ('!');
 
 client.on('message', message => {
-    if(message.content === prefix + 'ممكن لو سمحت يا بوتي اللذوذ تسويلي الوان يا قلبي ') {
+    if(message.content === prefix + ('ممكن لو سمحت يا بوتي اللذوذ تسويلي الوان يا قلبي ') {
                          if(!message.channel.guild) return message.channel.send('**This Commnad only For Servers !**'); 
          if(!message.member.hasPermission('ADMINISTRATOR')) return    message.channel.send('**You Dont Have** `ADMINISTRATOR` **premission**').then(msg => msg.delete(6000))
       message.guild.createRole({

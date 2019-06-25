5247af37f1d0499cd4fd17d29f44a4e7f01cfd48
kingdsakfj <42422746+kingdsakfj@users.noreply.github.com>
2019-06-25 05:43:09 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -47,32 +47,16 @@ client.on("ready", () => {
 
 
 
-client.on("guildMemberAdd", (member) => {
-    let channel = member.guild.channels.get("592732288824115241");
-    if (!channel) {
-        console.log("!the channel id it's not correct");
-        return;
-    }
-    if (member.id == client.user.id) {
-        return;
-    }
-    console.log('-');
-    var guild;
-    while (!guild)
-        guild = client.guilds.get("509014360950046736");
-    guild.fetchInvites().then((data) => {
-        data.forEach((Invite, key, map) => {
-            var Inv = Invite.code;
-            if (dat[Inv])
-                if (dat[Inv] < Invite.uses) {
- channel.send(`تم دعوته بواسطة  ${Invite.inviter} `) ;         
- }
-            dat[Inv] = Invite.uses;
-       
-       });
-    });
+client.on('guildMemberAdd', member => {
+  member.guild.fetchInvites().then(guildInvites => {
+    const ei = invites[member.guild.id];
+    invites[member.guild.id] = guildInvites;
+    const invite = guildInvites.find(i => ei.get(i.code).uses < i.uses);
+    const inviter = client.users.get(invite.inviter.id);
+    const logChannel = member.guild.channels.find(channel => channel.name === "𝐖𝐄𝐋𝐂𝐎𝐌𝐄-ٺــرحــيب");
+    logChannel.send(`${member} Invited by: <@${inviter.id}>`);
+  });
 });
-
 
 
 

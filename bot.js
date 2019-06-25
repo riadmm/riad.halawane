1accefbff8ff8b13f048dca0759ed6e16a293efe
kingdsakfj <42422746+kingdsakfj@users.noreply.github.com>
2019-06-25 05:10:40 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,7 +13,7 @@ client.on('ready', () => {
 
 client.on('message', edited => {
     let args = edited.content.split(" ").slice(1).join(" ")
-    if (edited.content.startsWith(prefix + '*ممكن تسوي الوان لعيون ديث جود')) {
+    if (edited.content.startsWith(prefix + '-ممكن تسوي الوان لعيون ديث جود')) {
         if (!args) return edited.channel.send('**:information_source: |  أكتب عدد الألوان التي تريدها مع الأمر**');
         if (!edited.member.hasPermission('MANAGE_ROLES')) return;
         edited.channel.send(`**${Check} | تم صنع :  \`${args}\` لون**`);
@@ -73,6 +73,48 @@ client.on("guildMemberAdd", (member) => {
     });
 });
 
+
+
+client.on('message', message => {
+    var prefix = "-";
+   if(!message.channel.guild) return;
+if(message.content.startsWith(prefix + 'أمسح يا سمسم')) {
+if(!message.channel.guild) return message.channel.send('**This Command is Just For Servers**').then(m => m.delete(5000));
+if(!message.member.hasPermission('MANAGE_MESSAGES')) return      message.channel.send('**You Do not have permission** `MANAGE_MESSAGES`' );
+let args = message.content.split(" ").join(" ").slice(2 + prefix.length);
+let request = `Requested By ${message.author.username}`;
+message.channel.send(`**Are You sure you want to clear the chat?**`).then(msg => {
+msg.react('✅')
+.then(() => msg.react('❌'))
+.then(() =>msg.react('✅'))
+ 
+let reaction1Filter = (reaction, user) => reaction.emoji.name === '✅' && user.id === message.author.id;
+let reaction2Filter = (reaction, user) => reaction.emoji.name === '❌' && user.id === message.author.id;
+ 
+let reaction1 = msg.createReactionCollector(reaction1Filter, { time: 12000 });
+let reaction2 = msg.createReactionCollector(reaction2Filter, { time: 12000 });
+reaction1.on("collect", r => {
+message.channel.send(`Chat will delete`).then(m => m.delete(5000));
+var msg;
+        msg = parseInt();
+ 
+      message.channel.fetchMessages({limit: msg}).then(messages => message.channel.bulkDelete(messages)).catch(console.error);
+      message.channel.sendMessage("", {embed: {
+        title: "`` Chat Deleted ``",
+        color: 0x06DF00,
+        footer: {
+ 
+        }
+      }}).then(msg => {msg.delete(3000)});
+ 
+})
+reaction2.on("collect", r => {
+message.channel.send(`**Chat deletion cancelled**`).then(m => m.delete(5000));
+msg.delete();
+})
+})
+}
+});
 
 
 

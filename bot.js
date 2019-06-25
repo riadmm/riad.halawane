544d78c2018bf3b0fbb4ccfaa551e1934e58dd97
kingdsakfj <42422746+kingdsakfj@users.noreply.github.com>
2019-06-25 04:04:34 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,15 @@ const client = new Discord.Client();
 
 
 
-  
+  client.on('message', message => {
+  if(message.content === prefix + '*ورع سوي الوان لا أصنك كف') {
+  if(!message.channel.guild) return message.channel.send('**This Commnad only For Servers !**');
+let menu = new Discord.RichEmbed()
+.setImage('https://www6.0zz0.com/2019/05/22/14/265175773.png')
+.setFooter('Colors Menu')
+message.channel.sendEmbed(menu)
+}})
+
 
 		
 

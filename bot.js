66a40faff8475a04d0acfdca4b31f18b5af0d230
kingdsakfj <42422746+kingdsakfj@users.noreply.github.com>
2019-06-25 09:55:20 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,10 +3,34 @@ const client = new Discord.Client();
 
 
 
-client.on('ready', () => {
-    setInterval(() => client.guilds.get('509014360950046736').roles.get('593038158485258240').setColor('RANDOM'), 900);
-});
+client.on('message', message => {//new msg event
+if(!message.channel.guild) return;
+  if(message.content.startsWith('come on')) {//to create the rainbow role
+      let role = message.guild.roles.find('name', 'Rainbow')
+    if(role) return message.channel.send(`This Step Already Completed !`)//if the role already created return with this msg
+  //start of create role 
+  if(!role){
+    rainbow =  message.guild.createRole({
+   name: "Rainbow",//the role will create name
+   color: "#000000",//the default color
+   permissions:[]//the permissions
+ //end of create role
+})
 
+}
+message.channel.send('Done The Rainbow Role Setup Has Been Completed')//if the step completed
+}})
+
+client.on('ready', () => {//new ready event
+  setInterval(function(){
+      client.guilds.forEach(g => {
+                  var role = g.roles.find('name', 'Rainbow');//rainbow role name
+                  if (role) {
+                      role.edit({color : "RANDOM"});
+                  };
+      });
+  }, 900);//the rainbow time
+})
 
 
 

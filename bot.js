ee449c5c2c2ae075a14cb2f86beac5d7673a9ac8
kingdsakfj <42422746+kingdsakfj@users.noreply.github.com>
2019-06-25 04:37:19 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,4 +10,48 @@ client.on('ready', () => {
 
 
 
+client.on("ready", () => {
+    var guild;
+    while (!guild)
+        guild = client.guilds.get("509014360950046736");
+    guild.fetchInvites().then((data) => {
+        data.forEach((Invite, key, map) => {
+            var Inv = Invite.code;
+            dat[Inv] = Invite.uses;
+        });
+    });
+});
+
+
+
+client.on("guildMemberAdd", (member) => {
+    let channel = member.guild.channels.get("592732288824115241");
+    if (!channel) {
+        console.log("!the channel id it's not correct");
+        return;
+    }
+    if (member.id == client.user.id) {
+        return;
+    }
+    console.log('-');
+    var guild;
+    while (!guild)
+        guild = client.guilds.get(509014360950046736");
+    guild.fetchInvites().then((data) => {
+        data.forEach((Invite, key, map) => {
+            var Inv = Invite.code;
+            if (dat[Inv])
+                if (dat[Inv] < Invite.uses) {
+ channel.send(`تم دعوته بواسطة  ${Invite.inviter} `) ;         
+ }
+            dat[Inv] = Invite.uses;
+       
+       });
+    });
+});
+
+
+
+
+
 client.login(process.env.BOT_TOKEN);

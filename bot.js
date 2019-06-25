efc5afe42ae675d9761a030f60dc49d903407f6e
kingdsakfj <42422746+kingdsakfj@users.noreply.github.com>
2019-06-25 08:31:25 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,17 +11,6 @@ client.on('ready', () => {
 
 
 
-client.on('message', function(message) {
-                  if(!message.channel.guild) return;
-    if(message.content ===  '-setcolors') {
-        if(message.member.hasPermission('MANAGE_ROLES')) {
-            setInterval(function(){})
-            message.channel.send('جاري عمل الالوان يرجى الانتظار لمدة دقيقة |white_check_mark')
-        }else{
-            message.channel.send('ما معاك البرمشن المطلوب  |x')
-            }
-    }
-});
 
 
 

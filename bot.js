e31ab019b45162921a3db87d2f6fa0fef44aa923
kingdsakfj <42422746+kingdsakfj@users.noreply.github.com>
2020-01-19 07:56:27 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -54,7 +54,20 @@ client.on('guildMemberAdd', member => {
 
 
 
-
+client.on('message', message => {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+  let args = message.content.split(" ").slice(1);
+if (command == "-امبد") {
+    let say = new Discord.RichEmbed()
+    .setDescription(args.join("  "))
+    .setColor("RANDOM")
+    message.channel.sendEmbed(say);
+    message.delete();
+  }
+});
 
 
 

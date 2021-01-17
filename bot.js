ba0946fe86a7a4841808642f10b0a1d4de85e1cb
riadmm <41975080+riadmm@users.noreply.github.com>
2021-01-17 18:53:56 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,19 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.on("message", message => {
 
-            if (message.content.startsWith(prefix + "!bc")) {
-                         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
-  let args = message.content.split(" ").slice(1);
-  var argresult = args.join(' '); 
-  message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {
- m.send(`${argresult}\n ${m}`);
-})
- message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'online').size}\` : ** عدد الاعضاء المستلمين ** `); 
- message.delete(); 
-};     
-});
 
 
 
